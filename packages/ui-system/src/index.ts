cd44c4d6656e62a57a261999c077e5255b932af0
Reza Negarestani <13441247+rezanet@users.noreply.github.com>
2026-03-14 21:50:38 +1100

    Update index.ts

---
+++ packages/ui-system/src/index.ts
@@ -0,0 +1,6 @@
+// Export Utility functions
+export { cn } from "./lib/utils";
+
+// Export Components
+export { Button, buttonVariants } from "./components/Button";
+export type { ButtonProps } from "./components/Button";
